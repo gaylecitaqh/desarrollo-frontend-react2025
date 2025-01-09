2858e9990d4155a076f46b2cbd5ba7c7ae9284e1
gaylecitaqh <gaylecitaqh@gmail.com>
2025-01-08 21:56:16 -0400

    Labo 5

--- src/Components/Core/OpenLink.jsx
+++ src/Components/Core/OpenLink.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line react/prop-types
 const OpenLink = ({redirectURL,LogoImage}) =>{
     return(
-        <div>
-                        
-                        
+        <>
+                 
                 <a href={redirectURL} >
                     <img  width="300" height="200"  src={LogoImage} className="Logo" alt="Vite logo" />
                 </a>
-        </div>
+        
+        </>
     );
 };
 
